ba711ce54651f41ea9baf33e9063c79250b34740
sourav.sharma <sourav.sharma@netsmartz.net>
2023-06-28 11:39:25 +0530

    splash added

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,17 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import Router from './router';
 import {SafeAreaProvider} from 'react-native-safe-area-context';
 import RNSafeView from './components/RNSafeView';
 import {Provider} from 'react-redux';
 import {store} from './feature/store';
 import ConnectionInfo from './utils/hooks/netInfo';
+import SplashScreen from 'react-native-splash-screen';
 
 function Main(): JSX.Element {
+  useEffect(() => {
+    SplashScreen.hide();
+  }, []);
+
   return (
     <SafeAreaProvider>
       <Provider store={store}>
